2faa24d4563398d44e421c74fb22d0d9b4596ccb
JK-Kim4 <jongbell4@gmail.com>
2023-09-25 13:45:21 +0900

    feat: first init

---
+++ src/main/java/com/tutomato/cloudeuruka/CloudeurukaApplication.java
@@ -0,0 +1,13 @@
+package com.tutomato.cloudeuruka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudeurukaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudeurukaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tutomato/cloudeuruka/CloudeurukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutomato.cloudeuruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudeurukaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
